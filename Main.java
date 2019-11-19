62447ae7aa63c47c986fa33a94864f6f24bd2a5f
19950512 <36517599+19950512@users.noreply.github.com>
2019-11-19 20:56:27 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,160 @@
+
+import java.util.*;
+import java.io.IOException;
+
+
+import Financeiro.Financeiro;
+import Financeiro.Menu;
+import Financeiro.Cliente;
+
+class Main {
+
+	public static void main(String[] args) {
+
+		limpaTerminal();
+		String closeBox = "\n--------------------------------------------------------------\n";
+
+        Menu menu = new Menu();
+		
+        Scanner in = new Scanner(System.in);
+		in.useLocale(Locale.ENGLISH);
+
+  		Financeiro a = new Financeiro();
+
+  		Cliente b = new Cliente();
+
+  		String cliente = null;
+
+		try{
+			while(true){
+
+				Double valor = 0d;
+
+				System.out.println(menu.tipoConta());
+				System.out.print("Digite a opcao desejada: ");
+				int acao = in.nextInt();
+				limpaTerminal();
+
+				// verificacao de tipo de conta
+				b.setFisico(false);
+				if (acao == 1) {
+					b.setFisico(true);
+				}
+
+				System.out.println(menu.login());
+				System.out.println(" 1 - ja sou cliente\n 2 - criar conta\n");
+				System.out.print("opcao: ");
+				int opLogar = in.nextInt();
+
+				limpaTerminal();
+				if(opLogar == 1) {
+					// logar cliente ja cadastrado
+
+					System.out.println(menu.login());
+					System.out.print("Informe seu ID para Logar: ");
+					int indentificacao = in.nextInt();
+					cliente = b.getNomeCliente(indentificacao);
+
+				} else if(opLogar == 2){
+					// cadastrar novo cliente
+
+					System.out.println(menu.login());
+					System.out.print("Informe seu nome: ");
+					String novoNome = in.nextLine();
+					b.novoCliente(novoNome);
+					System.out.println();
+					System.out.println("Bem Vindo " + novoNome);
+					System.out.println("Seu ID: " + b.novoClienteID() + "\n");
+				}
+				
+				limpaTerminal();
+				System.out.println(menu.boasvindas(cliente));
+				
+				while(true) {
+
+					// Opcoes para usuario ja logado
+					System.out.println(menu.principal());
+					System.out.print("Digite a opcao desejada: ");
+					int opcao = in.nextInt();
+
+					// CONSULTA
+					if (opcao == 1) {
+						limpaTerminal();
+						System.out.print(menu.saldo(a.getSaldo()));
+					}
+
+					// DEPOSITAR
+					else if (opcao == 2) {
+						try{
+							limpaTerminal();
+							System.out.println(menu.depositar());
+							System.out.print("Depositar valor: ");
+							valor = in.nextDouble();
+							System.out.print(closeBox);
+							a.Entrada(valor);
+						}catch(Exception e){
+							throw new Exception("Valor Inválido");
+						}
+					}
+
+					// EXTRATOS
+					else if (opcao == 3) {
+						limpaTerminal();
+						ArrayList<Double> movimentacoes = a.getMovimentacoes();
+						System.out.println(menu.movimentacoes());
+						System.out.println(" Mostrando Extrato de " + cliente + ":");
+						System.out.println(" Custo de uso: " + (b.getAdicional() * 100) + "%");
+						String tipoMov = "";
+
+						for (Double trans : movimentacoes) {
+							if(trans > 0){
+								tipoMov = " Depositado: ";
+							} else if(trans < 0) {
+								trans = trans * -1;
+								tipoMov = " Retirado: ";
+							} else {
+								tipoMov = " Movimentacao invalida: ";
+							}
+
+							System.out.println(tipoMov + "R$ " + trans);
+						}
+						System.out.print(closeBox);
+					}
+
+					// SACAR
+					else if (opcao == 4) {
+
+						limpaTerminal();
+						Double valorIn = a.getValor();
+						
+						System.out.println(menu.sacar());
+						System.out.print(" Sacar valor: ");
+						try{
+							valor = in.nextDouble();
+						}catch(Exception e){
+							throw new Exception("Valor Inválido");
+						}
+						if(valor > valorIn){
+							System.out.println(" Transacao invalida");
+						}
+						System.out.print(closeBox);
+						a.Saida(valor);
+					}
+
+					// SAIR
+					else if (opcao == 0) {
+						limpaTerminal();
+						break;
+					}
+				}
+			}
+  		}catch(Exception e){
+			System.out.print(e.getMessage());
+		}
+  	}
+	
+	public static void limpaTerminal(){
+		System.out.print("\033[H\033[2J");  
+		System.out.flush(); 
+	}
+}
